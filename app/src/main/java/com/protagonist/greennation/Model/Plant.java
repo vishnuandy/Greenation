3dfdd7a43bb4d9aae3b845331a73b0c35b4437c2
vishnu <vishnulingam2394@gmail.com>
2016-12-17 15:04:12 +0530

    GridLayout design

---
+++ app/src/main/java/com/protagonist/greennation/Model/Plant.java
@@ -0,0 +1,27 @@
+package com.protagonist.greennation.Model;
+
+/**
+ * Created by Santhosh R on 17-12-2016.
+ */
+
+public class Plant {
+    private String plant_name;
+
+    public String getPlant_image() {
+        return plant_image;
+    }
+
+    public void setPlant_image(String plant_image) {
+        this.plant_image = plant_image;
+    }
+
+    public String getPlant_name() {
+        return plant_name;
+    }
+
+    public void setPlant_name(String plant_name) {
+        this.plant_name = plant_name;
+    }
+
+    private String plant_image;
+}
